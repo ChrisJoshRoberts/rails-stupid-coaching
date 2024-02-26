53a032935d5846d4b8d9be077bb6ad11d6f744cf
Chris Roberts <chrisrobertscollective@gmail.com>
2024-02-26 14:34:58 +0200

    stupid coach in rails

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,5 @@
+class AnswersController < ApplicationController
+  def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+  end
+end
